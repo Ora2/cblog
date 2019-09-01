178f422065c61f6147fb8f35b751d0fa873a32b1
lyd <984689593@qq.com.com>
2019-09-01 22:34:48 +0800

    Initial commit

---
+++ src/test/java/co/happymeal/cblog/CblogApplicationTests.java
@@ -0,0 +1,16 @@
+package co.happymeal.cblog;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CblogApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
